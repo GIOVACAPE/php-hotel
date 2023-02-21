11c90297387bdf79f36e80c79d0c51d76029a733
Giovanni Capelli <giovacape01@gmail.com>
2023-02-21 13:19:30 +0100

    COMPLETATO

--- index.php
+++ index.php
@@ -52,6 +52,7 @@ $vote = $_GET['vote'] ?? null;
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/svg+xml" href="logo.png" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <title>Hotel di GC</title>
 
